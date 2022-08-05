e9d8b0eb17645814c3c638113af7083e0084be63
aayush-aryan <log2aayusharyan@gmail.com>
2022-08-05 23:40:39 +0530

    added. welcome message for MoodAnalyser

---
+++ MoodAnalyserConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MoodAnalyserConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To MoodAnalyser Console Application!");
+        }
+    }
+}
