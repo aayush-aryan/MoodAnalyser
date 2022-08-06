c751739471908b47cdbfdad14c429e99b1b4c5bd
aayush-aryan <log2aayusharyan@gmail.com>
2022-08-06 23:41:38 +0530

    added. a method to analyseMood and all testcases pass successfully

---
+++ MoodAnalyserConsoleApp/AnalyzeMood.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MoodAnalyserConsoleApp
+{
+   public class AnalyzeMood
+    {
+        public string Mood(string message)
+        {
+            if (message.ToLower().Contains("happy"))
+            {
+                return "happy";
+            }
+            else
+            {
+                return "sad";
+            }
+        }
+    }
+}
