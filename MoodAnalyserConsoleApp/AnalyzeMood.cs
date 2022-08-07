8e2874364502263208c9a4108c1aae98742394ed
aayush-aryan <log2aayusharyan@gmail.com>
2022-08-07 13:29:27 +0530

    refactored. refactoredUC1 to take mood message in Constructor

--- MoodAnalyserConsoleApp/AnalyzeMood.cs
+++ MoodAnalyserConsoleApp/AnalyzeMood.cs
@@ -4,9 +4,48 @@ using System.Text;
 
 namespace MoodAnalyserConsoleApp
 {
-   public class AnalyzeMood
+    public class AnalyzeMood
     {
+        //variable
+        public string message;
+        /// <summary>
+        /// parameterized constructor
+        /// </summary>
+        /// <param name="message"></param>
+        public AnalyzeMood(string message)
+        {
+            this.message = message;
+        }
+        /// <summary>
+        /// default Constructor
+        /// </summary>
+        public AnalyzeMood()
+        {
+        }
+        //uc1
+        /// <summary>
+        /// Method to return the type of Mood
+        /// </summary>
+        /// <param name="message"></param>
+        /// <returns></returns>
         public string Mood(string message)
+        {
+            if (message.ToLower().Contains("happy"))
+            {
+                return "happy";
+            }
+            else
+            {
+                return "sad";
+            }
+        }
+        //UC1_Refactored
+        /// <summary>
+        /// refactored Mood
+        /// metod overloading
+        /// </summary>
+        /// <returns></returns>
+        public string Mood()
         {
             if (message.ToLower().Contains("happy"))
             {
